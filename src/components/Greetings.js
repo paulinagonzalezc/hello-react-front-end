264b865941b01220318c732d4c26d91177c521da
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-04-04 19:05:08 -0600

    Add routes to app.js

---
+++ src/components/Greetings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Greetings() {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <p>This is the greetings page.</p>
+    </div>
+  );
+}
+
+export default Greetings;
